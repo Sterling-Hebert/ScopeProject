548b6cb1587b6716304cc1a403d7a92bb7dfd758
sterling_hebert <sterling_hebert@outlook.com>
2022-07-14 19:56:52 -0500

    finished 0-11

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -9,14 +9,30 @@ console.log(rudePerson("how are you")); // prints "how what are what you"
 console.log(rudePerson("I like pie")); // prints "I what like what pie"
 
 
-Invoking the interrupter function again: 
+Invoking the interrupter function again:
 let rudePerson2 = interrupter("yo"); // => returns a function
 console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 
 ***********************************************************************/
 
-// your code here!
+function interrupter(interruptingWord) {
+  return function (sentence) {
+    let words = sentence.split(" ");
+    let newString = "";
+
+    for (let index = 0; index < words.length; index++) {
+      let word = words[index];
+      if (index === words.length - 1) {
+        newString += word;
+      } else {
+        newString += word + " " + interruptingWord + " ";
+      }
+    }
+
+    return newString;
+  };
+}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
